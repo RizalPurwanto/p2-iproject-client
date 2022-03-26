45730d3263adbe19fc4992725c10717d5f1390ff
RizalPurwanto <rizal.purwanto.id@gmail.com>
2022-03-26 12:54:02 +0700

    added wizard for form

--- iproject/src/main.js
+++ iproject/src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
+import VueFormWizard from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueFormWizard)
 new Vue({
   router,
   store,
